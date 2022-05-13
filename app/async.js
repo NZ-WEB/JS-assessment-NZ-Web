c7fea55fe3dbae2a14eb8046d8a4b373aad9da79
NZ-WEB <stereo00@yandex.ru>
2022-05-13 16:22:37 +0300

    Added best practics

--- app/async.js
+++ app/async.js
@@ -1,11 +1,19 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
-
+  async : function(value) {
+    return Promise.resolve(value);
   },
 
-  manipulateRemoteData: function(url) {
-
+  manipulateRemoteData : function(url) {
+    return new Promise(function (resolve, reject) {
+      $.ajax(url).done(function (results) {
+        resolve(results.people.map(function (person) {
+          return person.name;
+        }).sort(function (x, y) {
+          return x > y;
+        }));
+      }).fail(reject);
+    });
   }
 };
